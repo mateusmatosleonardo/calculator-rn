da5564bfbe8393c0dc496d4e5ec59812b15e0349
Mateus Leonardo <m4teusmatos@gmail.com>
2021-10-15 13:40:23 -0300

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { 
+    StyleSheet,
+    Text,
+    Dimensions,
+    TouchableHighlight
+} from "react-native";
+
+const styles = StyleSheet.create({
+    button:{
+        fontSize: 40,
+        height: Dimensions.get('window').width / 4
+    }
+})
